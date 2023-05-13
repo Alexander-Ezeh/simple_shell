e697e1bf4d631e984adbf57788c551bb5e93c05b
Alexander_Ezeh <alexanderikennaefunaezeh@gmail.com>
2023-05-13 14:29:27 -0700

    main.c

---
+++ main.c
@@ -0,0 +1,73 @@
+#include "main.h"
+
+/**
+ * main - function frees data structure
+ *
+ * @datash: Pointer to the data structure to be freed
+ * Return: This function does not return a value
+ */
+void free_data(data_shell *datash)
+{
+	unsigned int i;
+
+	for (i = 0; datash->_environ[i]; i++)
+	{
+		free(datash->_environ[i]);
+	}
+
+	free(datash->_environ);
+	free(datash->pid);
+}
+
+/**
+ * main - function initialize data structure
+ *
+ * @datash: Pointer to the data structure to
+ * @av: Argument vector
+ * Return: This function does not return a value
+ */
+void set_data(data_shell *datash, char **av)
+{
+	unsigned int i;
+
+	datash->av = av;
+	datash->input = NULL;
+	datash->args = NULL;
+	datash->status = 0;
+	datash->counter = 1;
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	datash->_environ = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+	{
+		datash->_environ[i] = _strdup(environ[i]);
+	}
+
+	datash->_environ[i] = NULL;
+	datash->pid = aux_itoa(getpid());
+}
+
+/**
+ * main - entry point of the program
+ *
+ * @ac: Number of command-line arguments passed to the program
+ * @av: Array of strings containing the command-line arguments
+ *
+ * Return: 0 on success.
+ */
+int main(int ac, char **av)
+{
+	data_shell datash;
+	(void) ac;
+
+	signal(SIGINT, get_sigint);
+	set_data(&datash, av);
+	shell_loop(&datash);
+	free_data(&datash);
+	if (datash.status < 0)
+		return (255);
+	return (datash.status);
+}
