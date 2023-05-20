071ec9509c5cce791ff576b0aa0fb0540f02fb3c
Alexander_Ezeh <alexanderikennaefunaezeh@gmail.com>
2023-05-20 02:11:55 -0700

    builtins_1 & 2

---
+++ builtins_2.c
@@ -0,0 +1,35 @@
+#include "shell.h"
+
+/**
+ * builtins_list - searches match and executes associate builtin
+ * @data: program's data structure
+ * Return: Returns the function executed if there is a match,
+ * else returns -1.
+ **/
+int builtins_list(data_of_program *data)
+{
+	int iterator;
+	builtins options[] = {
+		{"exit", builtin_exit},
+		{"help", builtin_help},
+		{"cd", builtin_cd},
+		{"alias", builtin_alias},
+		{"env", builtin_env},
+		{"setenv", builtin_set_env},
+		{"unsetenv", builtin_unset_env},
+		{NULL, NULL}
+	};
+
+/*walk through the structure*/
+	for (iterator = 0; options[iterator].builtin != NULL; iterator++)
+	{
+/*if matched between given command and builtin,*/
+		if (str_compare(options[iterator].builtin, data->command_name, 0))
+		{
+/*execute function, and return value of function*/
+			return (options[iterator].function(data));
+		}
+/*if no match return -1 */
+	}
+	return (-1);
+}
